af624abf395be5fc2cb55c290a931965a2834eb9
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-09-18 21:43:42 -0400

    Added enums for button layout and colors

---
+++ Twitter/Constants/ButtonLayout.swift
@@ -0,0 +1,15 @@
+//
+//  ButtonLayout.swift
+//  Twitter
+//
+//  Created by Denielle Abaquita on 9/18/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public enum ButtonLayout {
+    static let roundedCornerRadius: CGFloat = 15
+    static let largeButtonFont: UIFont = UIFont.boldSystemFont(ofSize: 30)
+}
---
+++ Twitter/Constants/Colors.swift
@@ -0,0 +1,18 @@
+//
+//  Colors.swift
+//  Twitter
+//
+//  Created by Denielle Abaquita on 9/18/21.
+//  Copyright © 2021 Dan. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public enum Colors {
+    static let blue = UIColor(named: "Twitter Blue")
+    static let darkGrey = UIColor(named: "Dark Grey")
+    static let lightGrey = UIColor(named: "Light Grey")
+    static let extraLightGrey = UIColor(named: "Extra Light Grey")
+    static let extraLightGrey2 = UIColor(named: "Extra Extra Light Grey")
+}
