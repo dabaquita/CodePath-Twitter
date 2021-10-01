593665b02bab356601bfb6a15339c6cc6c3534d7
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-01 14:10:02 -0400

    Configured text view for tweets

--- Twitter/ViewControllers/CreateTweetViewController.swift
+++ Twitter/ViewControllers/CreateTweetViewController.swift
@@ -39,6 +39,7 @@ class CreateTweetViewController: UIViewController {
         )
         
         // Load subviews
+        configureTweetTextView()
     }
 
     @objc func didTapCancel(_ sender: Any?) {
@@ -51,4 +52,47 @@ class CreateTweetViewController: UIViewController {
     }
     
     // Format subviews
+    func configureTweetTextView() {
+        tweetTextView.delegate = self
+        tweetTextView.text = "New Tweet..."
+        tweetTextView.textColor = UIColor.lightGray
+        tweetTextView.font = .systemFont(ofSize: 17)
+        view.addSubview(tweetTextView)
+        
+        // Constraints
+        tweetTextView.translatesAutoresizingMaskIntoConstraints = false
+        NSLayoutConstraint.activate([
+            tweetTextView.topAnchor.constraint(equalTo: view.topAnchor, constant: 12),
+            tweetTextView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 12),
+            tweetTextView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -12),
+            tweetTextView.bottomAnchor.constraint(equalTo: view.bottomAnchor, constant: -12)
+        ])
+    }
+}
+
+extension CreateTweetViewController: UITextViewDelegate {
+    func textView(_ textView: UITextView, shouldChangeTextIn range: NSRange, replacementText text: String) -> Bool {
+        
+        // Combine textView text and replacement
+        let currentText: String = tweetTextView.text
+        let updatedText = (currentText as NSString).replacingCharacters(in: range, with: text)
+        
+        // Add placeholder and set cursor to beginning of text view
+        if updatedText.isEmpty {
+            textView.text = "New Tweet..."
+            textView.textColor = UIColor.lightGray
+            
+            textView.selectedTextRange = textView.textRange(
+                from: textView.beginningOfDocument,
+                to: textView.beginningOfDocument
+            )
+        } else if textView.textColor == UIColor.lightGray && !text.isEmpty {
+            textView.textColor = .black
+            textView.text = text
+        } else {
+            return true
+        }
+        
+        return false
+    }
 }
