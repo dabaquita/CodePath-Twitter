7d0baf5c7dbcd25c3e8e81bc3f19921fbe3a53da
Denielle Abaquita <denielle.abaquita@gmail.com>
2021-10-02 10:18:55 -0400

    Added DEBUG env var to help with rate limit programming

--- Twitter/ViewControllers/HomeViewController.swift
+++ Twitter/ViewControllers/HomeViewController.swift
@@ -85,6 +85,10 @@ class HomeViewController: UITableViewController {
                 self.refreshControl?.endRefreshing()
             },
             failure: { error in
+                if ProcessInfo.processInfo.environment["DEBUG"] == "1" {
+                    self.tweetDict.append(NSDictionary())
+                    self.tableView.reloadData()
+                }
                 print("Could not retrieve tweets due to \(error)")
             }
         )
@@ -128,25 +132,24 @@ class HomeViewController: UITableViewController {
             return UITableViewCell()
         }
         
+        // Set default values for cell
+        cell.profileNameLabel.text = "Name Here"
+        cell.tweetLabel.text = "Content Here"
+        cell.profileImageView.image = UIImage(systemName: "person.fill")
+        
         let tweet = tweetDict[indexPath.row]
         guard let user = tweet["user"] as? NSDictionary else {
             return cell
         }
         
-        cell.profileImageView.image = UIImage(systemName: "pin")
-        
         // Profile Name
         if let name = user["name"] as? String {
             cell.profileNameLabel.text = name
-        } else {
-            cell.profileNameLabel.text = "Name Here"
         }
     
         // Tweet content
         if let tweetContent = tweet["text"] as? String {
             cell.tweetLabel.text = tweetContent
-        } else {
-            cell.tweetLabel.text = "Content Here"
         }
         
         // Profile Image
@@ -164,7 +167,7 @@ class HomeViewController: UITableViewController {
     }
     
     override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return view.bounds.height / 8
+        return view.bounds.height / 7
     }
     
     override func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
